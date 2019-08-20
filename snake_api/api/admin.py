b31cf2d7531ffb04b738296ed33885bb965187b9
Merry Cimakasky <mkcimakasky@gmail.com>
2019-08-19 17:21:59 -0700

    registered models

--- snake_api/api/admin.py
+++ snake_api/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Flight, Passenger, Seat
 
 # Register your models here.
+
+admin.site.register(Flight)
+admin.site.register(Passenger) 
+admin.site.register(Seat)
