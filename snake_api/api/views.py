7b1d75f2ffefc2ab12e2a5ec4b4ac77ab82952e8
Merry Cimakasky <mkcimakasky@gmail.com>
2019-08-20 14:36:19 -0700

    adding CRUD

--- snake_api/api/views.py
+++ snake_api/api/views.py
@@ -4,26 +4,26 @@ from rest_framework import generics
 from .models import Flight, Passenger, Seat
 from snake_api.serializers import SnakeApiSeralizer
 
-class FlightList(generics.ListAPIView): 
+class FlightList(generics.ListCreateAPIView): 
   queryset = Flight.objects.all() 
   seralizer_class = SnakeApiSeralizer
 
-class FlightDetail(generics.RetrieveAPIView): 
+class FlightDetail(generics.RetrieveUpdateDestroyAPIView): 
   queryset = Flight.objects.all() 
   seralizer_class = SnakeApiSeralizer
 
-class PassengerList(generics.ListAPIView): 
+class PassengerList(generics.ListCreateAPIView): 
   queryset = Flight.objects.all() 
   seralizer_class = SnakeApiSeralizer
 
-class PassengerDetail(generics.RetrieveAPIView): 
+class PassengerDetail(generics.RetrieveUpdateDestroyAPIView): 
   queryset = Flight.objects.all()
   seralizer_class = SnakeApiSeralizer
 
-class SeatList(generics.ListAPIView): 
+class SeatList(generics.ListCreateAPIView): 
   queryset = Seat.objects.all()
   seralizer_class = SnakeApiSeralizer
 
-class SeatDetail(generics.RetrieveAPIView): 
+class SeatDetail(generics.RetrieveUpdateDestroyAPIView): 
   queryset = Flight.objects.all() 
   seralizer_class = SnakeApiSeralizer
