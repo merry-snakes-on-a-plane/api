74a52ce57b3119c47bd475a541d291793a38427c
Merry Cimakasky <mkcimakasky@gmail.com>
2019-08-29 16:21:17 -0700

    mostly built out, need logic for creating a grid

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Flight, Passenger, Seat
+from .models import Flight, Passenger, Seat, Cell, Player
 
 # Register your models here.
 
 admin.site.register(Flight)
 admin.site.register(Passenger) 
 admin.site.register(Seat)
+admin.site.register(Cell)
+admin.site.register(Player)
+
--- api/models.py
+++ api/models.py
@@ -13,3 +13,11 @@ class Seat(models.Model):
   name = models.CharField(max_length=256) 
   flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name="flight")
   passenger = models.ForeignKey(Passenger, on_delete=models.SET_DEFAULT, default=None, related_name='passenger')
+
+class Cell(models.Model): 
+	type_of_cell = models.CharField(max_length=100)
+	habitable = models.BooleanField(default=True)
+
+class Player(models.Model): 
+	current_cell = models.ForeignKey(Cell, on_delete=models.PROTECT, related_name='location')
+	
